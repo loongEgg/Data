dacbde86360321cf89253012a960e3942ed45534
loongEgg <loongEgg@users.noreply.github.com>
2021-02-22 21:55:26 +0800

    [Update] 1. Add Reset() method in DataSeries, an alternate implement of AddRange.

--- LoongEgg.Data/DataSeries.cs
+++ LoongEgg.Data/DataSeries.cs
@@ -1,11 +1,43 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 
 namespace LoongEgg.Data
 {
     /// <summary>
     /// 数据序列
     /// </summary>
-    public class DataSeries : ObservableCollection<Point> {
-         public string Name { get; set; }
+    public class DataSeries : ObservableCollection<Point>
+    {
+        /// <summary>
+        /// Name of this DataSeries
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 横坐标范围
+        /// </summary>
+        public Range Xrange { get; set; }
+
+        /// <summary>
+        /// 纵坐标范围
+        /// </summary>
+        public Range Yrange { get; set; }
+
+        /// <summary>
+        /// 重置为指定的集合
+        /// </summary>
+        /// <param name="points"></param>
+        public void Reset(IEnumerable<Point> points)
+        {
+            if (points == null) return;
+            Items.Clear();
+
+            foreach (var p in points)
+            {
+                Items.Add(p);
+            }
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
     }
 }
